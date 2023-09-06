859fee41dc4f528daa07c37454da9c49f7dacac1
khosi <cosmasndulue00611@gmail.com>
2023-09-06 20:41:27 +0100

    a

---
+++ images/src/data.js
@@ -0,0 +1,23 @@
+let shopItemsData = [{
+    id: "item1",
+    name: "casual shirt",
+    price: 65,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img:"./nimble-made-BKYeLLB1OxI-unsplash.jpg"
+
+},{ id:"item2",
+    name: "official shirt",
+    price: 95,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img:"./benjamin-rascoe-KF-q_SGqswg-unsplash.jpg"
+},{ id:"item3",
+    name: "cool shirt",
+    price: 200,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img:"./faith-yarn-Wr0TpKqf26s-unsplash.jpg"
+},{ id:"item4",
+    name: "brown shirt",
+    price: 210,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+    img:"./jonathon-dorofy-Qne1wCsWfUA-unsplash.jpg"
+}];
