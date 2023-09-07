36f7289d4515d7860195437d71fc964cf35b6018
khosi <cosmasndulue00611@gmail.com>
2023-09-07 15:06:58 +0100

    a

--- images/src/data.js
+++ images/src/data.js
@@ -24,10 +24,60 @@ let shopItemsData = [{
 
 {
     id:"item5",
-    name:"top",
-    price: 150,
-    desc: "loremsndskdjkfknflsdhjuwue",
+    name:"jean jacket top",
+    price: 170,
+    desc: "loremsndskdjkfknflsdhjuwu",
     img: "./img5.jpg"
+},
+{
+    id:"item6",
+    name:"summer top",
+    price: 120,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img6.jpg"
+},
+{
+    id:"item7",
+    name:"white casual top",
+    price: 50,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img7.jpg"
+},
+{
+    id:"item8",
+    name:"blue casual top",
+    price: 150,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img8.jpg"
+},
+{
+    id:"item9",
+    name:"3 in one",
+    price: 300,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img9.jpg"
+},
+{
+    id:"item11",
+    name:"white pullover",
+    price: 90,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img11.jpg"
+},
+
+{
+    id:"item54",
+    name:"brown jacket",
+    price: 200,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img54.jpg"
+},
+{
+    id:"item99",
+    name:"pairs of jeans",
+    price: 130,
+    desc: "loremsndskdjkfknflsdhjuwu",
+    img: "./img99.jpg"
 }
 
 ];
